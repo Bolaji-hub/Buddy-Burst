a29a57f9402d3b03988433f19832d32f368fce75
Barakat Bolaji <barakatbolaji65@gmail.com>
2024-01-18 12:44:22 +0100

    - Install React router don - Install react icons - import raleway fonts - creating components ans pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Goals from "./pages/Goals";
+import Connect from "./pages/Connect";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/goal" element={<Goals />} />
+          <Route path="/connect" element={<Connect />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
